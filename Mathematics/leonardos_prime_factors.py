ecfcfa83dafe387de82430a3b63099cad52a9438
Joshua Dunne <40161967+MeVsTheVoices@users.noreply.github.com>
2023-05-21 04:22:34 -0700

    finished with leonardos

--- Mathematics/leonardos_prime_factors.py
+++ Mathematics/leonardos_prime_factors.py
@@ -21,6 +21,47 @@ def wilsons_theorem(n):
 def generate_primes(n):
     return [i for i in range(1, n) if wilsons_theorem(i)]
 
+def generate_n_primes(n):
+    primes = []
+    i = 1
+    while len(primes) < n:
+        if wilsons_theorem(i):
+            primes.append(i)
+        i += 1
+    return primes
+
+def generate_n_primes_static(n):
+    primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
+103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 
+223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 
+347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 
+463, 467, 479, 487, 491, 499, 503, 509, 521, 523]
+    return primes[:n]
+
+def all_multiples_less_than_n(j, n):
+    return [i*j for i in range(1, n // j)]
+
+def eratosthenes_sieve(n):
+    primes = []
+    multiples = []
+    for i in range(2, n):
+        if i not in multiples:
+            primes.append(i)
+            multiples.extend(all_multiples_less_than_n(i, n))
+    return primes
+
+def primeCount(n):
+    if (n == 1):
+        return 0
+    number_of_primes = 1
+    primes = generate_n_primes_static(number_of_primes)
+    while math.prod(primes) <= n:
+        number_of_primes += 1
+        primes = generate_n_primes_static(number_of_primes)
+    return number_of_primes - 2
+
+
+
 if __name__ == '__main__':
     #specific to hackerrank
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -38,3 +79,4 @@ if __name__ == '__main__':
         fptr.write(str(result) + '\n')
 
     fptr.close()
+
--- Mathematics/test_leonardos_prime_factors.py
+++ Mathematics/test_leonardos_prime_factors.py
@@ -17,3 +17,22 @@ def test_wilsons_theorem():
 
 def test_generate_primes():
     assert leonardos_prime_factors.generate_primes(24) == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+def test_all_multiples_less_than_n():
+    assert leonardos_prime_factors.all_multiples_less_than_n(2, 10) == [2, 4, 6, 8]
+
+def test_eratosthenes_sieve():
+    assert leonardos_prime_factors.eratosthenes_sieve(24) == [2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+def test_generate_n_primes():
+    assert leonardos_prime_factors.generate_n_primes(10) == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23]
+    assert leonardos_prime_factors.generate_n_primes(9) == [1, 2, 3, 5, 7, 11, 13, 17, 19]
+    assert leonardos_prime_factors.generate_n_primes(8) == [1, 2, 3, 5, 7, 11, 13, 17]
+    assert leonardos_prime_factors.generate_n_primes(7) == [1, 2, 3, 5, 7, 11, 13]
+    assert leonardos_prime_factors.generate_n_primes(6) == [1, 2, 3, 5, 7, 11]
+    assert leonardos_prime_factors.generate_n_primes(5) == [1, 2, 3, 5, 7]
+    assert leonardos_prime_factors.generate_n_primes(4) == [1, 2, 3, 5]
+    assert leonardos_prime_factors.generate_n_primes(3) == [1, 2, 3]
+    assert leonardos_prime_factors.generate_n_primes(2) == [1, 2]
+    assert leonardos_prime_factors.generate_n_primes(1) == [1]
+    
