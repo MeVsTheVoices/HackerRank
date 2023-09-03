e94de69dc11b54eeec7e8a5a5e0b55e6d908b62f
Joshua Dunne <mevsthevoices@gmail.com>
2023-09-03 13:52:15 -0700

    getting started on k sorted lists

---
+++ cs497/assignment1/mergeksortedlists/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        // there is a more optimum solution than the one I'm about to implement
+        // we'd use a priority queue to find the next list to draw elements from
+        
+    }
+}
