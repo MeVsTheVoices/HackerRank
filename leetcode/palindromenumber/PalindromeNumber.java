3126e26a530b546ba4f6c7a412559e1f47be205e
Joshua Dunne <mevsthevoices@gmail.com>
2023-09-21 17:25:12 -0700

    three out of five done

---
+++ leetcode/palindromenumber/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package leetcode.palindromenumber;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        long multiple = 1;
+        while(Math.floorDiv(x, multiple) > 0) {
+            multiple *= 10;
+        }
+        multiple /= 10;
+        System.out.println(multiple);
+        
+        long lowMultiple = 10;
+        boolean isPalindrome = true;
+        while (multiple >= lowMultiple) {
+            long upperTerm = (x / multiple) % 10;
+            long lowerTerm = (x % lowMultiple) / (lowMultiple / 10);
+
+            System.out.println(upperTerm + " " + lowerTerm);
+
+            if (upperTerm != lowerTerm)
+                isPalindrome = false;
+            
+            lowMultiple *= 10;
+            multiple /= 10;
+        }
+  
+        
+
+        return isPalindrome;
+    }
+}
