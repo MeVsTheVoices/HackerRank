4d89c5f104eaa286801b2aa9b76add25ad8c0a79
Joshua Dunne <mevsthevoices@gmail.com>
2023-09-21 18:02:56 -0700

    completed 4 of 5

--- cs497/assignment2/maximumgap/Solution.java
+++ cs497/assignment2/maximumgap/Solution.java
@@ -2,11 +2,11 @@ package cs497.assignment2.maximumgap;
 
 class Solution {
     public int maximumGap(int[] nums) {
-        int[] newNums = new int[nums.length];
+        /*int[] newNums = new int[nums.length];
         if (nums.length < 2)
             return 0;
         boolean wasStill = false;
-        while was Stille
+        while was Stille*/
         return 0;
     }
 }
