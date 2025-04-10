4301785d56daf00413b2114d298c9852073989ff
Joshua Dunne <mevsthevoices@gmail.com>
2025-04-09 17:08:45 -0700

    misc

--- leetcode/listnode.py
+++ leetcode/listnode.py
@@ -2,3 +2,24 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    def from_list(lst):
+        if not lst:
+            return None
+        head = ListNode(lst[0])
+        current = head
+        for i in range(1, len(lst)):
+            current.next = ListNode(lst[i])
+            current = current.next
+        return head
+    
+    def __eq__(self, value):
+        # Compare the values of the linked list
+        current1 = self
+        current2 = value
+        while current1 and current2:
+            if current1.val != current2.val:
+                return False
+            current1 = current1.next
+            current2 = current2.next
+        return current1 is None and current2 is None
